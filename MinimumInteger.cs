1680e323ec6511cac21230678c54dbd457c7e356
Madhur82954 <itzmadhurverma@gmail.com>
2023-09-29 15:17:57 +0530

    [Madhur]-day5-Practice Added

---
+++ MinimumInteger.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_Practice
+{
+    class checkforGeneric<T> where T : IComparable
+    {
+        public T n1, n2, n3;
+        public checkforGeneric(T n1,T n2,T n3)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+            this.n3 = n3;
+        }
+        public void checkeveryvalue() 
+        {
+            if (n1.CompareTo(n2) < 0 && n1.CompareTo(n3) < 0)
+            {
+                Console.WriteLine("n1 is Minimum");
+            }
+            else if (n2.CompareTo(n1) < 0 && n2.CompareTo(n3) < 0)
+            {
+                Console.WriteLine("n2 is Minimum");
+            }
+            else if (n3.CompareTo(n1) < 0 && n3.CompareTo(n2) < 0)
+            {
+                Console.WriteLine("n3 is Minimum");
+            }
+        }
+    }
+    internal class MinimumInteger
+    {
+        public void checkMin() 
+        {
+            int n1, n2, n3;
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            n3 = int.Parse(Console.ReadLine());
+            if(n1<n2 && n1 < n3)
+            {
+                Console.WriteLine("n1 is Minimum");
+            }
+            else if (n2 < n1 && n2 < n3)
+            {
+                Console.WriteLine("n2 is Minimum");
+            }
+            else if (n3 < n1 && n3 < n2)
+            {
+                Console.WriteLine("n3 is Minimum");
+            }
+        }
+        public void checkMin1()
+        {
+            float n1, n2, n3;
+            n1 = float.Parse(Console.ReadLine());
+            n2 = float.Parse(Console.ReadLine());
+            n3 = float.Parse(Console.ReadLine());
+            if (n1 < n2 && n1 < n3)
+            {
+                Console.WriteLine("n1 is Minimum");
+            }
+            else if (n2 < n1 && n2 < n3)
+            {
+                Console.WriteLine("n2 is Minimum");
+            }
+            else if (n3 < n1 && n3 < n2)
+            {
+                Console.WriteLine("n3 is Minimum");
+            }
+        }
+        public void checkMin2()
+        {
+            string n1, n2, n3;
+            n1 = Console.ReadLine();
+            n2 = Console.ReadLine();
+            n3 = Console.ReadLine();
+            if (n1.CompareTo(n2) < 0 && n1.CompareTo(n3) < 0)
+            {
+                Console.WriteLine("n1 is Minimum");
+            }
+            else if (n2.CompareTo(n1) < 0 && n2.CompareTo(n3) < 0)
+            {
+                Console.WriteLine("n2 is Minimum");
+            }
+            else if (n3.CompareTo(n1) < 0 && n3.CompareTo(n2) < 0)
+            {
+                Console.WriteLine("n3 is Minimum");
+            }
+        }
+        public void checkeveryvalue<T>(T n1,T n2,T n3) where T:IComparable
+        {
+            if (n1.CompareTo(n2) < 0 && n1.CompareTo(n3) < 0)
+            {
+                Console.WriteLine("n1 is Minimum");
+            }
+            else if (n2.CompareTo(n1) < 0 && n2.CompareTo(n3) < 0)
+            {
+                Console.WriteLine("n2 is Minimum");
+            }
+            else if (n3.CompareTo(n1) < 0 && n3.CompareTo(n2) < 0)
+            {
+                Console.WriteLine("n3 is Minimum");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day5_Practice
+{
+    class DeleteArray<T> where T : IComparable
+    {
+        public void Add(params T[] values)
+        {
+            var list = new List<T>();
+            list.AddRange(values);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void Delete1(T values)
+        {
+            var list = new List<T>();
+            list.Remove(values);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    internal class Program
+    {
+        public void Delete()
+        {
+            var list = new List<int>();
+            var list1 = new List<double>();
+            var list2 = new List<char>();
+
+            list.Add(1);
+            list.Add(4);
+            list.Add(5);
+            list.Add(8);
+
+            foreach(var element in list)
+            {
+                Console.WriteLine(element);
+            }
+            list.Remove(5);
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+            list1.Add(1.14);
+            list1.Add(4.14);
+            list1.Add(5.14);
+            list1.Add(8.14);
+
+            foreach (var element in list1)
+            {
+                Console.WriteLine(element);
+            }
+            list1.Remove(4.14);
+            foreach (var element in list1)
+            {
+                Console.WriteLine(element);
+            }
+            list2.Add('a');
+            list2.Add('b');
+            list2.Add('c');
+            list2.Add('d');
+
+            foreach (var element in list2)
+            {
+                Console.WriteLine(element);
+            }
+            list2.Remove('c');
+            foreach (var element in list2)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        public void Add<T>(params T[] values)
+        {
+            var list = new List<T>();
+            list.AddRange(values);
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void Delete1<T>(T values)
+        {
+            var list = new List<T>();
+            list.Remove(values);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Program program = new Program();
+            //program.Delete();
+            //program.Add(1,4,5,8);
+            //program.Delete1(5);
+
+            //MinimumInteger minimum = new MinimumInteger();
+            //minimum.checkMin2();
+            //minimum.checkeveryvalue<int>(2,1,3);
+
+            checkforGeneric<int> checkfor = new checkforGeneric<int>(4,3,1);
+            checkfor.checkeveryvalue();
+        }
+    }
+}
